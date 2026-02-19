7c87e84b6b20dfa91615ad3d1df853cf7621b15a
Sebastian Dettlaff <sdettlaff679@gmail.com>
2026-02-19 10:54:29 +0000

    Refactor ContactController to use RequestBody

--- backend/src/main/java/com/example/contactform/controller/ContactController.java
+++ backend/src/main/java/com/example/contactform/controller/ContactController.java
@@ -1,38 +1,3 @@
-//package com.example.contactform.controller;
-//
-//import com.example.contactform.dto.ContactFormDto;
-//import com.example.contactform.service.CaptchaValidator;
-//import com.example.contactform.service.EmailService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.*;
-//
-//
-//@RestController
-//public class ContactController {
-//
-//    @Autowired
-//    private CaptchaValidator captchaValidator;
-//
-//    @Autowired
-//    private EmailService emailService;
-//    @CrossOrigin(origins = "*") // tutaj https://dctiptop.co.uk
-//    @PostMapping("/send-message")
-//    public ResponseEntity<?> handleForm(@ModelAttribute ContactFormDto dto) {
-//        if (!captchaValidator.validate(dto.getRecaptchaToken())) {
-//            return ResponseEntity.badRequest().body("Captcha mismatch, try again.");
-//        }
-//        emailService.sendContactEmail(dto);
-//        return ResponseEntity.ok("Message sent successfully");
-//    }
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity<String> handleException(Exception ex) {
-//        ex.printStackTrace(); // wypisz w konsoli
-//        return ResponseEntity.status(500).body("Internal error: " + ex.getMessage());
-//    }
-//}
 package com.example.contactform.controller;
 
 import com.example.contactform.dto.ContactFormDto;
@@ -74,4 +39,5 @@ public class ContactController {
         ex.printStackTrace();
         return ResponseEntity.status(500).body("Internal error: " + ex.getMessage());
     }
+
 }
